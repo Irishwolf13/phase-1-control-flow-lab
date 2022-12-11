44757723139a92458ca8be9795ba5afb0623b987
John Rooney <rooneyjohn@gmail.com>
2022-12-10 21:50:17 -0500

    commit tree cc8a8ba0fbf4e03609648effd279123f4e723c70

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= '400') {
+    return 'This one is on me!'
+  }
+  if ( value <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+  if (value <= 2500 ) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  if (value > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(value){
+  return (value == 'NYC' ? "Ok, sounds good." : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
 }
+console.log(switchOnCharmFromTip())
